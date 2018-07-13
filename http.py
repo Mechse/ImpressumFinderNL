7606884fe5628bc5c7c2858279b6e5a09539dd14
Max Leodolter <max.leo2000@gmail.com>
2018-07-13 14:25:28 +0200

    fith commit

--- http.py
+++ http.py
@@ -5,7 +5,7 @@ def getContent(url):
     try:
         page = requests.get('http://' + url)
     except:
-        return "Site can't be reached."
+        return
 
     content = page.content
     return content
--- main.py
+++ main.py
@@ -8,7 +8,34 @@ fh = FileHandler()
 domains = db.getURLs(10)
 for domain in domains:
     content = http.getContent(domain)
-    soup = BeautifulSoup(content, "lxml")
+
+    try:
+        soup = BeautifulSoup(str(content), "lxml")
+    except TypeError:
+        print("Can't make soup.")
+        continue
+
     if Regex().searchImprints(soup.prettify()):
         if fh.inFile("somethingFound.txt", domain) == -1:
             fh.writeSomethingFound(domain+"\n")
+
+    for link in soup.findAll('a'):
+        hyperlink = link.get('href')
+        hyper_content = http.getContent(hyperlink)
+
+        if hyper_content is not None:
+            print hyperlink
+            hyperlink_soup = BeautifulSoup(str(hyper_content), "lxml")
+
+        else:
+            hyperlink = domain+hyperlink
+            hyper_content = http.getContent(hyperlink)
+            try:
+                hyperlink_soup = BeautifulSoup(str(hyper_content), "lxml")
+            except TypeError:
+                print("Can't make soup.")
+                continue
+
+        if Regex().searchImprints(hyperlink_soup.prettify()):
+            if fh.inFile("somethingFound.txt", hyperlink) == -1:
+                fh.writeSomethingFound(hyperlink+"\n")
