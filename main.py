fe6343915ba41821676b60b777c31271f241ad5b
Max Leodolter <max.leo2000@gmail.com>
2018-07-13 15:39:38 +0200

    sixth commit

--- main.py
+++ main.py
@@ -19,23 +19,4 @@ for domain in domains:
         if fh.inFile("somethingFound.txt", domain) == -1:
             fh.writeSomethingFound(domain+"\n")
 
-    for link in soup.findAll('a'):
-        hyperlink = link.get('href')
-        hyper_content = http.getContent(hyperlink)
-
-        if hyper_content is not None:
-            print hyperlink
-            hyperlink_soup = BeautifulSoup(str(hyper_content), "lxml")
-
-        else:
-            hyperlink = domain+hyperlink
-            hyper_content = http.getContent(hyperlink)
-            try:
-                hyperlink_soup = BeautifulSoup(str(hyper_content), "lxml")
-            except TypeError:
-                print("Can't make soup.")
-                continue
-
-        if Regex().searchImprints(hyperlink_soup.prettify()):
-            if fh.inFile("somethingFound.txt", hyperlink) == -1:
-                fh.writeSomethingFound(hyperlink+"\n")
+    print(Regex().searchData(soup.prettify()))
--- regex.py
+++ regex.py
@@ -9,3 +9,9 @@ class Regex():
         for key in fh.readImprintKeys():
             regex = re.compile(str(key))
             return regex.findall(input)
+
+    def searchData(self, input):
+        fh = FileHandler()
+        for regex_input in fh.readRegex():
+            regex = re.compile(regex_input)
+            return regex.findall(input)
