effbc236bf8f3f5225afc4ab5702aba0db5c53ae
Max Leodolter <max.leo2000@gmail.com>
2018-07-26 14:09:22 +0200

    first db extension

---
+++ db.py
@@ -0,0 +1,17 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="",           # your host, usually localhost
+                     user="",                   # your username
+                     passwd="",   # your password
+                     db="")                     # name of the data base
+
+
+def GetDomains(sql):
+    domains = list()
+
+    cur = db.cursor()
+
+    cur.execute(sql)
+
+    for row in cur.fetchall():
+        domains.append(row)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import csv
 import functions as f
+import db
 
 
 
